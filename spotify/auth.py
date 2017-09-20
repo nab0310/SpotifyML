2b12acf97cfb2ac0ef7c1df922f7c3698c83223c
Nick Behrens <behrensn@iastate.edu>
2017-09-19 21:05:23 -0500

    Initial Commit

---
+++ spotify/auth.py
@@ -0,0 +1,25 @@
+
+# shows a user's saved tracks (need to be authenticated via oauth)
+
+import sys
+import spotipy
+import spotipy.util as util
+
+scope = 'user-library-read'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
